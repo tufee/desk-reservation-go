4d0a1f09056efce17ae1e68ebc2a6ed716fe6f6b
Paulo Almeidaactivities-cli <pauloalmeida.jrr@gmail.com>
2025-05-26 21:29:04 -0300

    run migrate when init

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
 	"github.com/tufee/desk-reservation-go/internal/api"
 )
 
 func init() {
-
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Warning: .env file not found")
+	}
+}
+
+func init() {
+	db, err := sql.Open(os.Getenv("POSTGRES_DB"), os.Getenv("CONNECTION_STRING"))
+	if err != nil {
+		log.Fatal("Error opening database:", err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal("Error creating postgres driver:", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		os.Getenv("MIGRATION_DIR"),
+		os.Getenv("DRIVER"),
+		driver,
+	)
+	if err != nil {
+		log.Fatal("Error initializing migration instance:", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("Error executing migrations:", err)
 	}
 }
 
