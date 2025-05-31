add014b2e46e33b3160ce521a8fbbee947babcd8
Paulo Almeidaactivities-cli <pauloalmeida.jrr@gmail.com>
2025-05-31 12:19:14 -0300

    refactor user handler

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -3,11 +3,7 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"log"
 	"net/http"
-	"time"
-
-	"github.com/go-playground/validator/v10"
 
 	"github.com/tufee/desk-reservation-go/internal/domain"
 	"github.com/tufee/desk-reservation-go/internal/service"
@@ -18,7 +14,7 @@ import (
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	var data domain.CreateUser
 
-	if err := parseAndValidateRequest(r, &data, w); err != nil {
+	if err := pkg.ParseAndValidateRequest(r, &data, w); err != nil {
 		return
 	}
 
@@ -26,27 +22,7 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.WithValue(r.Context(), utils.CreateUserKey, user)
 
 	if err := service.CreateUserService(ctx); err != nil {
-		w.Header().Set("Content-Type", "application/json")
-
-		switch e := err.(type) {
-		case *pkg.BadRequestError:
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]any{
-				"message": e.Error(),
-			})
-
-		case *pkg.InternalServerError:
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]any{
-				"message": e.Error(),
-			})
-
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]any{
-				"message": "Internal server error",
-			})
-		}
+		pkg.HandleHTTPError(w, err)
 		return
 	}
 
@@ -57,50 +33,10 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func parseAndValidateRequest(
-	r *http.Request,
-	data *domain.CreateUser,
-	w http.ResponseWriter,
-) error {
-	if err := json.NewDecoder(r.Body).Decode(data); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return err
-	}
-
-	validate := validator.New(validator.WithRequiredStructEnabled())
-	if err := validate.Struct(data); err != nil {
-		log.Println("CreateUser Validation error")
-		respondWithValidationErrors(w, err.(validator.ValidationErrors))
-		return err
-	}
-	return nil
-}
-
 func buildUserFromRequest(data domain.CreateUser) domain.User {
-	now := time.Now()
 	return domain.User{
-		Name:       data.Name,
-		Email:      data.Email,
-		Password:   data.Password,
-		Created_at: now,
-		Updated_at: now,
+		Name:     data.Name,
+		Email:    data.Email,
+		Password: data.Password,
 	}
-}
-
-func respondWithValidationErrors(w http.ResponseWriter, val validator.ValidationErrors) {
-	errors := []map[string]string{}
-	for _, err := range val {
-		errors = append(errors, map[string]string{
-			"field": err.Field(),
-			"tag":   err.Tag(),
-		})
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusBadRequest)
-	json.NewEncoder(w).Encode(map[string]any{
-		"status":  "error",
-		"message": "Validation failed",
-		"errors":  errors,
-	})
 }
--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -1,6 +1,13 @@
 package utils
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type BadRequestError struct {
 	Message string
@@ -31,5 +38,66 @@ func NewInternalServerError(message string, err error) *InternalServerError {
 	return &InternalServerError{
 		Message: message,
 		Err:     err,
+	}
+}
+
+func ParseAndValidateRequest[T any](
+	r *http.Request,
+	data *T,
+	w http.ResponseWriter,
+) error {
+	if err := json.NewDecoder(r.Body).Decode(data); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return err
+	}
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(data); err != nil {
+		log.Println("Validation struct error")
+		RespondWithValidationErrors(w, err.(validator.ValidationErrors))
+		return err
+	}
+	return nil
+}
+
+func RespondWithValidationErrors(w http.ResponseWriter, val validator.ValidationErrors) {
+	errors := []map[string]string{}
+	for _, err := range val {
+		errors = append(errors, map[string]string{
+			"field": err.Field(),
+			"tag":   err.Tag(),
+		})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(map[string]any{
+		"status":  "error",
+		"message": "Validation failed",
+		"errors":  errors,
+	})
+}
+
+func HandleHTTPError(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "application/json")
+
+	switch e := err.(type) {
+	case *BadRequestError:
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]any{
+			"message": e.Error(),
+		})
+
+	case *InternalServerError:
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]any{
+			"message": e.Error(),
+		})
+
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]any{
+			"message": "Internal server error",
+		})
 	}
 }
